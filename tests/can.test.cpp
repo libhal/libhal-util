956bb9496e3aef068d84c243936e7c8d896e604f
Khalil Estell <kammce0@gmail.com>
2024-12-21 18:11:40 -0800

    :sparkles: Add can_message_finder

--- tests/can.test.cpp
+++ tests/can.test.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 
 #include <boost/ut.hpp>
+#include <libhal/can.hpp>
+#include <ratio>
 
 namespace hal {
 
@@ -71,6 +73,40 @@ void check_validity(hal::hertz p_operating_frequency,
          static_cast<std::uint32_t>(p_target_baud_rate))
     << "Failure to get the expected baud rate with " << test_subject;
 }
+
+struct test_can_transceiver : hal::can_transceiver
+{
+  test_can_transceiver() = default;
+  hal::u32 driver_baud_rate() override
+  {
+    using namespace hal::literals;
+    return 100_kHz;
+  }
+
+  void driver_send(hal::can_message const& p_message) override
+  {
+    last_sent_message = p_message;
+  }
+
+  std::span<hal::can_message const> driver_receive_buffer() override
+  {
+    return m_buffer;
+  }
+
+  std::size_t driver_receive_cursor() override
+  {
+    return m_cursor % m_buffer.size();
+  };
+
+  void add_to_received_messages(hal::can_message const& p_message)
+  {
+    m_buffer[m_cursor++ % m_buffer.size()] = p_message;
+  }
+
+  hal::can_message last_sent_message{};
+  std::array<hal::can_message, 12> m_buffer{};
+  std::size_t m_cursor = 0;
+};
 }  // namespace
 
 namespace hal {
@@ -159,5 +195,171 @@ boost::ut::suite<"can_test"> can_test = [] {
       expect(that % not fail2.has_value());
       expect(that % not fail4.has_value());
     };
+
+  "hal::can_message_finder"_test = []() {
+    "hal::can_message_finder::find() two messages"_test = []() {
+      // Setup
+      constexpr hal::u32 search_id = 0x115;
+      test_can_transceiver test_transceiver;
+      hal::can_message_finder reader(test_transceiver, search_id);
+      hal::can_message findable_message0{
+        .id = search_id,
+        .extended = false,
+        .remote_request = false,
+        .length = 3,
+        .payload = { 0xAB, 0xCD, 0xEF },
+      };
+
+      hal::can_message findable_message1 = {
+        .id = search_id,
+        .extended = false,
+        .remote_request = false,
+        .length = 2,
+        .payload = { 0xDE, 0xAD },
+      };
+
+      hal::can_message skipped_message = {
+        .id = search_id + 5,
+        .extended = false,
+        .remote_request = false,
+        .length = 1,
+        .payload = { 0xCC },
+      };
+
+      // Ensure
+      auto const ensure_message = reader.find();
+      expect(not ensure_message.has_value());
+
+      // Exercise
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(findable_message0);
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(findable_message1);
+      test_transceiver.add_to_received_messages(findable_message1);
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(findable_message0);
+      test_transceiver.add_to_received_messages(skipped_message);
+      auto const found_message0 = reader.find();
+      auto const found_message1 = reader.find();
+      auto const found_message2 = reader.find();
+      auto const found_message3 = reader.find();
+      auto const no_message_found0 = reader.find();
+      auto const no_message_found1 = reader.find();
+
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(findable_message1);
+      test_transceiver.add_to_received_messages(skipped_message);
+
+      auto const found_message4 = reader.find();
+
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(skipped_message);
+      test_transceiver.add_to_received_messages(skipped_message);
+
+      auto const no_message_found2 = reader.find();
+
+      // Verify
+      expect(found_message0.has_value());
+      expect(found_message0.value() == findable_message0);
+      expect(found_message1.has_value());
+      expect(found_message1.value() == findable_message1);
+      expect(found_message2.has_value());
+      expect(found_message2.value() == findable_message1);
+      expect(found_message3.has_value());
+      expect(found_message3.value() == findable_message0);
+      expect(found_message4.has_value());
+      expect(found_message4.value() == findable_message1);
+      expect(not no_message_found0.has_value());
+      expect(not no_message_found1.has_value());
+      expect(not no_message_found2.has_value());
+    };
+
+    "hal::can_message_finder::find() overflow cursor"_test = []() {
+      // Setup
+      constexpr hal::u32 expected_id = 0x115;
+      test_can_transceiver test_transceiver;
+      hal::can_message_finder reader(test_transceiver, expected_id);
+      hal::can_message desired_message{
+        .id = expected_id,
+        .extended = false,
+        .remote_request = false,
+        .length = 3,
+        .payload = { 0xAB, 0xCD, 0xEF },
+      };
+
+      hal::can_message undesired_message = desired_message;
+      undesired_message.id = expected_id + 1;
+      // Setup: With 8 messages to fill the buffer, the cursor will land back at
+      // 0. Meaning that a find will come up with nothing. This is to be
+      // expected with a circular buffer if the receive cursor gets lapped.
+
+      // Ensure
+      auto const ensure_message0 = reader.find();
+      expect(not ensure_message0.has_value());
+      auto const ensure_message1 = reader.find();
+      expect(not ensure_message1.has_value());
+
+      // Exercise
+      test_transceiver.add_to_received_messages(desired_message);
+      for (std::size_t i = 0; i < test_transceiver.m_buffer.size(); i++) {
+        test_transceiver.add_to_received_messages(undesired_message);
+      }
+
+      auto const found_message0 = reader.find();
+
+      // Verify
+      expect(not found_message0.has_value());
+    };
+
+    "hal::can_message_finder::find() std::nullopt from stability failure"_test =
+      []() {
+        // This is empty because we haven't come up with a practical cross
+        // platform way to verify this check.
+      };
+
+    "hal::can_message_finder::transceiver() should work"_test = []() {
+      // Setup
+      test_can_transceiver test_transceiver;
+      hal::can_message_finder reader(test_transceiver, 0x111);
+      constexpr hal::can_message expected_message{
+        .id = 0x111,
+        .extended = false,
+        .remote_request = false,
+        .length = 3,
+        .payload = { 0xAB, 0xCD, 0xEF },
+      };
+      // Setup: With 8 messages to fill the buffer, the cursor will land back at
+      // 0. Meaning that a find will come up with nothing. This is to be
+      // expected with a circular buffer if the receive cursor gets lapped.
+
+      // Exercise
+      reader.transceiver().send(expected_message);
+
+      // Verify
+      expect(expected_message == test_transceiver.last_sent_message);
+    };
+
+    "hal::can_message_finder::id()"_test = []() {
+      // Setup
+      constexpr auto desired_id = 0x087;
+      test_can_transceiver test_transceiver;
+      hal::can_message_finder reader(test_transceiver, desired_id);
+
+      // Exercise
+      auto const actual_id = reader.id();
+
+      // Verify
+      expect(desired_id == actual_id);
+    };
+  };
 };
 }  // namespace hal
