2615a3e2f77a61c6385fab4b895301723c278152
PhazonicRidley <ma13hew@gmail.com>
2025-02-28 11:03:29 -0800

    :wrench: Allow compile_commands.json to be generated without building entire project

--- conanfile.py
+++ conanfile.py
@@ -67,6 +67,11 @@ class libhal_util_conan(ConanFile):
     def layout(self):
         cmake_layout(self)
 
+    def generate(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build(target="copy_compile_commands")
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
